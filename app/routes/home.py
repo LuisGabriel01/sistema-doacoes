33ed1d49bbe400d4f7e4c2852d2816bee649eac7
LuisGabriel01 <luisgabrielqueiroz@gmail.com>
2026-03-03 21:38:24 -0300

    query user

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, render_template_string
+from flask import Blueprint, render_template_string, render_template
 from flask_security import Security, auth_required
+from sqlalchemy import select
+from app.models import User
+from app.database import db_session
 
 homes = Blueprint('home',__name__)
 
 @homes.route('/')
 @auth_required()
 def home():
-    return render_template_string('ola')
+    stmt = select(User)
+    query = db_session.scalars(stmt).all()
+    return render_template('index.html',query=query)
