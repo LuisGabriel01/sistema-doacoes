991e8a716d7c058940527ee89faf7758875093db
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-03-04 01:44:29 +0000

    populando a tabela Assistidos

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template_string, render_template
-from flask_security import Security, current_user, auth_required, hash_password, \
-    SQLAlchemySessionUserDatastore, permissions_accepted
+from flask_security import (
+    Security,
+    current_user,
+    auth_required,
+    hash_password,
+    SQLAlchemySessionUserDatastore,
+    permissions_accepted,
+)
 from app.database import db_session, init_db
 from app.models import User, Role
 from app.seed import seed_db_from_json
 from app.config import Config
 from app.routes.home import homes
+
 app = Flask(__name__)
 
 app.config.from_object(Config)
@@ -17,29 +24,29 @@ app.teardown_appcontext(lambda exc: db_session.close())
 user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
 security = Security(app, user_datastore)
 
-@app.cli.command('seed_db')
-def seed_db():
-    if User.query.first() is None:
-        seed_db_from_json(db_session)
-        print('populando banco de dados a partir do json')
-    else:
-        print('banco ja contem dados, para começar do zero apague o arquivo test.db')
 
-# one time setup
-with app.app_context():
-    init_db()
-    # Create a user and role to test with
-    security.datastore.find_or_create_role(
-        name="user", permissions={"user-read", "user-write"}
-    )
-    db_session.commit()
-    if not security.datastore.find_user(email="test@me.com"):
-        security.datastore.create_user(email="test@me.com",
-        password=hash_password("password"), roles=["user"])
-    db_session.commit()
+@app.cli.command("seed_db")
+def seed_db():
+    with app.app_context():
+        init_db()
+        # Create a user and role to test with
+        security.datastore.find_or_create_role(
+            name="user", permissions={"user-read", "user-write"}
+        )
+        db_session.commit()
+        if not security.datastore.find_user(email="test@me.com"):
+            security.datastore.create_user(
+                email="test@me.com", password=hash_password("password"), roles=["user"]
+            )
+        db_session.commit()
+
+        seed_db_from_json(db_session)
+        print("populando banco de dados a partir do json")
+
 
 app.register_blueprint(homes)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # run application (can also use flask run)
     app.run()
+
--- app/seed.py
+++ app/seed.py
@@ -1,8 +1,20 @@
-from sqlalchemy.orm import scoped_session
+from sqlalchemy import select
+from sqlalchemy.orm import Session, scoped_session
+from app.models import Assistido
+import json
 
-def seed_db_from_json(db_session: scoped_session, filename):
-    if filename is None:
-        filename = '../mock_data.json'
+
+def seed_db_from_json(db_session: scoped_session[Session]):
+    # if filename is None:
+    filename = "mock_data.json"
     with open(filename) as f:
-        pass
+        data = json.load(f)
+    # print(data)
+    for line in data:
+        # print(line, type(line))
+        assistido = Assistido(**line)
+        db_session.add(assistido)
 
+    print(*db_session.execute(select(Assistido)).all())
+    db_session.commit()
+    
