c2bb6c78aa8c6bbb1c2913760da69b2ab9301c4c
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-03-06 19:55:03 +0000

    ler [modelo].json para cada subclasse de "db"

--- app/seed.py
+++ app/seed.py
@@ -1,20 +1,29 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session, scoped_session
-from app.models import Assistido
+
+# from app.models import Assistido
+import app.models as models
+from app.database import db
 import json
 
 
 def seed_db_from_json(db_session: scoped_session[Session]):
-    # if filename is None:
-    filename = "mock_data.json"
-    with open(filename) as f:
-        data = json.load(f)
-    # print(data)
-    for line in data:
-        # print(line, type(line))
-        assistido = Assistido(**line)
-        db_session.add(assistido)
+    for model in db.__subclasses__():
+        name = model.__name__
+        filename = f"{name}.json"
+        with open(filename) as f:
+            data = json.load(f)
+            print(data)
 
-    print(*db_session.execute(select(Assistido)).all())
-    db_session.commit()
-    
+    # # if filename is None:
+    # filename = "mock_data.json"
+    # with open(filename) as f:
+    #     data = json.load(f)
+    # # print(data)
+    # for line in data:
+    #     # print(line, type(line))
+    #     assistido = Assistido(**line)
+    #     db_session.add(assistido)
+
+    # print(*db_session.execute(select(Assistido)).all())
+    # db_session.commit()
