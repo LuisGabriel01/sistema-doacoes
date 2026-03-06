d0e683c69f5408a2cd3e5ced1ee6955ebe909dae
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-03-06 21:34:05 +0000

    converter data_hora em datetime

--- app/seed.py
+++ app/seed.py
@@ -1,5 +1,8 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session, scoped_session
+from flask_security.models import sqla
+
+from datetime import datetime
 
 # from app.models import Assistido
 import app.models as models
@@ -9,11 +12,25 @@ import json
 
 def seed_db_from_json(db_session: scoped_session[Session]):
     for model in db.__subclasses__():
+        if issubclass(model, sqla.FsRoleMixin) or issubclass(model, sqla.FsUserMixin):
+            continue
         name = model.__name__
-        filename = f"{name}.json"
+        filename = f"mock_data/{name}.json"
         with open(filename) as f:
             data = json.load(f)
-            print(data)
+            # print(data)
+        for line in data:
+            # print(line, type(line))
+            if "data_hora" in line.keys():
+                line["data_hora"] = datetime.fromisoformat(line["data_hora"])
+                # print(line, line["data_hora"])
+
+            print(line)
+            row = model(**line)
+            db_session.add(row)
+        # print(*db_session.execute(select(model)).all())
+        db_session.commit()
+        # print(*db_session.execute(select(model)).all())
 
     # # if filename is None:
     # filename = "mock_data.json"
