59b587527ab1b0610d3a01a0a4da99217394e3d3
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-03-17 19:44:19 -0300

    remove comentarios antigos

--- app/seed.py
+++ app/seed.py
@@ -10,9 +10,7 @@ from app.database import Base
 from app.config import Config
 
 
-
 def seed_mock_from_json(db_session: scoped_session[Session]):
-
     for model in Base.__subclasses__():
         if issubclass(model, sqla.FsRoleMixin) or issubclass(model, sqla.FsUserMixin):
             continue
@@ -22,22 +20,7 @@ def seed_mock_from_json(db_session: scoped_session[Session]):
         for row in data:
             if "data_hora" in row.keys():
                 row["data_hora"] = datetime.fromisoformat(row["data_hora"])
-
             row = model(**row)
             db_session.add(row)
         db_session.commit()
         print(db_session.execute(select(model)).all())
-
-
-    # # if filename is None:
-    # filename = "mock_data.json"
-    # with open(filename) as f:
-    #     data = json.load(f)
-    # # print(data)
-    # for line in data:
-    #     # print(line, type(line))
-    #     assistido = Assistido(**line)
-    #     db_session.add(assistido)
-
-    # print(*db_session.execute(select(Assistido)).all())
-    # db_session.commit()
