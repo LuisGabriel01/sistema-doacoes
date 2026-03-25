052e865ed7a517183ad3aff3bcf475704c57cf43
Luiz Tosk <6207512+luiztosk@users.noreply.github.com>
2026-03-24 21:27:43 -0300

    formulario disable edit

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template_string, render_template
 from flask_security import Security, auth_required
 from sqlalchemy import select
-from app.models import User
+from app.models import User, Assistido
 from app.database import db_session
 
 homes = Blueprint('home',__name__)
@@ -10,5 +10,9 @@ homes = Blueprint('home',__name__)
 @auth_required()
 def home():
     stmt = select(User)
+    test = select(Assistido)
     query = db_session.scalars(stmt).all()
+    qt = db_session.scalars(test).all()
+    print(qt[0].__class__.__table__.columns.keys())
+    print(query[0].__class__.__table__.columns.keys())
     return render_template('index.html',query=query)
--- app/routes/registro.py
+++ app/routes/registro.py
@@ -1,25 +1,45 @@
 from flask import Blueprint, render_template, redirect
 from flask_security.decorators import auth_required
 
+from sqlalchemy import select
 from app.forms import AssistidoForm
 from app.database import db_session
 from app.models import Assistido
 
 registros = Blueprint('registro',__name__)
 
-@registros.route('/registro', methods=['GET', 'POST'])
+tables = {
+    'assistido': {
+        'form': AssistidoForm,
+        'model': Assistido,
+        'template': 'registro_assistido.html'
+    },
+    'doador': {
+        'form': AssistidoForm,
+        'model': Assistido,
+        'template': 'registro_assistido.html'
+    }
+}
+
+@registros.route('/registro/<table>/<int:id>', methods=['GET', 'POST'])
 @auth_required()
-def registro():
+def registro(table, id):
     form = AssistidoForm()
-    print(form.errors)
+    if id != 0:
+        stmt = select(Assistido).where(Assistido.id == id)
+        assist = db_session.scalars(stmt).all()[0]
+        ast = Assistido()
+        print(assist)
+        form = tables[table]['form'](obj=assist)
+        print(form.errors)
     if form.validate_on_submit():
         print('validado!!!')
         print(form.data)
         data = form.data
         data.pop('csrf_token')
         print(data)
-        row = Assistido(**data)
+        row = tables[table]['model'](**data)
         db_session.add(row)
         db_session.commit()
         return redirect('/') # /success
-    return render_template('registro_assistido.html', form=form)
+    return render_template(tables[table]['template'], form=form, disabled=True)
